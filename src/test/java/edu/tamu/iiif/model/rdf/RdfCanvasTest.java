5e1833933a20d339ab85a41b9bd30f09a17a224f
William Welling <wwelling@library.tamu.edu>
2018-04-23 16:10:18 -0500

    added rdf canvas test

--- src/test/java/edu/tamu/iiif/model/rdf/RdfCanvasTest.java
+++ src/test/java/edu/tamu/iiif/model/rdf/RdfCanvasTest.java
@@ -1,5 +1,34 @@
 package edu.tamu.iiif.model.rdf;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import de.digitalcollections.iiif.presentation.model.impl.v2.ImageImpl;
+
+@RunWith(SpringRunner.class)
 public class RdfCanvasTest {
+
+    @Test
+    public void testCreateDefault() {
+        RdfCanvas rdfCanvas = new RdfCanvas();
+        Assert.assertNotNull(rdfCanvas);
+        Assert.assertNotNull(rdfCanvas.getHeight());
+        Assert.assertNotNull(rdfCanvas.getWidth());
+        Assert.assertNotNull(rdfCanvas.getImages());
+        Assert.assertEquals(0, rdfCanvas.getImages().size());
+    }
+
+    @Test
+    public void testUpdate() {
+        RdfCanvas rdfCanvas = new RdfCanvas();
+        rdfCanvas.setHeight(100);
+        rdfCanvas.setWidth(100);
+        rdfCanvas.addImage(new ImageImpl());
+        Assert.assertEquals(100, rdfCanvas.getHeight());
+        Assert.assertEquals(100, rdfCanvas.getWidth());
+        Assert.assertEquals(1, rdfCanvas.getImages().size());
+    }
 
 }
