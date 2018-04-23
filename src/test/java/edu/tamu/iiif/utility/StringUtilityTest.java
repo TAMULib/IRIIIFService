57b263b8e9d19411c3273b198b607a68aae5d8ae
William Welling <wwelling@library.tamu.edu>
2018-04-23 16:17:02 -0500

    string utility tests

--- src/test/java/edu/tamu/iiif/utility/StringUtilityTest.java
+++ src/test/java/edu/tamu/iiif/utility/StringUtilityTest.java
@@ -1,5 +1,26 @@
 package edu.tamu.iiif.utility;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class StringUtilityTest {
+
+    @Test
+    public void testJoinPath() {
+        Assert.assertEquals("src/main/resources", StringUtility.joinPath("src", "main", "resources"));
+    }
+
+    @Test
+    public void testEncode() {
+        Assert.assertEquals("SGVsbG8sIFdvcmxkIQ==", StringUtility.encode("Hello, World!"));
+    }
+
+    @Test
+    public void testDecode() {
+        Assert.assertEquals("Hello, World!", StringUtility.decode("SGVsbG8sIFdvcmxkIQ=="));
+    }
 
 }
