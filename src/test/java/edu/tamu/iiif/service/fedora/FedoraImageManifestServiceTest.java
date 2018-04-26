e0acdf3b85be47c6ecc8c6810d5cfa4ec872ce54
William Welling <wwelling@library.tamu.edu>
2018-04-26 11:31:58 -0500

    fedora image manifest test

--- src/test/java/edu/tamu/iiif/service/fedora/FedoraImageManifestServiceTest.java
+++ src/test/java/edu/tamu/iiif/service/fedora/FedoraImageManifestServiceTest.java
@@ -1,14 +1,29 @@
 package edu.tamu.iiif.service.fedora;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class FedoraImageManifestServiceTest extends AbstractFedoraManifestServiceTest {
 
     @InjectMocks
     private FedoraImageManifestService fedoraImageManifestService;
+
+    @Value("classpath:mock/fedora/json/image.json")
+    private Resource image;
 
     @Before
     public void setup() {
@@ -21,8 +36,10 @@ public class FedoraImageManifestServiceTest extends AbstractFedoraManifestServic
     }
 
     @Test
-    public void testGetManifest() {
-
+    public void testGetManifest() throws IOException, URISyntaxException {
+        when(httpService.get(any(String.class))).thenReturn(FileUtils.readFileToString(image.getFile(), "UTF-8"));
+        String manifest = fedoraImageManifestService.getManifest("cars_pcdm_objects/chevy/pages/page_0/files/PTAR_800x400.png", false);
+        Assert.assertEquals(objectMapper.readValue(image.getFile(), JsonNode.class), objectMapper.readValue(manifest, JsonNode.class));
     }
 
 }
