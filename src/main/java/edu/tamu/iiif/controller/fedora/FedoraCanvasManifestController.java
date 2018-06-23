c35330fe3427756ab92af0992d526de9943039cf
William Welling <wwelling@outlook.com>
2018-06-23 17:48:44 -0500

    using get mapping, explicit on GET

--- src/main/java/edu/tamu/iiif/controller/fedora/FedoraCanvasManifestController.java
+++ src/main/java/edu/tamu/iiif/controller/fedora/FedoraCanvasManifestController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ import edu.tamu.iiif.service.fedora.FedoraCanvasManifestService;
 @RequestMapping("/" + FEDORA_IDENTIFIER)
 public class FedoraCanvasManifestController extends AbstractManifestController<FedoraCanvasManifestService> {
 
-    @RequestMapping("/" + CANVAS_IDENTIFIER + "/**/*")
+    @GetMapping("/" + CANVAS_IDENTIFIER + "/**/*")
     public void image(
         // @formatter:off
         HttpServletResponse response,
