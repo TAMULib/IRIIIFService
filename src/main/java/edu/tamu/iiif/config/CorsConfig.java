284c9f421ef51b864de5946f5f984f3b1d3c252b
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2019-04-22 10:03:42 -0500

    Changed parent to weaver and updated CorsConfig

--- src/main/java/edu/tamu/iiif/config/CorsConfig.java
+++ src/main/java/edu/tamu/iiif/config/CorsConfig.java
@@ -1,9 +1,7 @@
 package edu.tamu.iiif.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -22,7 +20,7 @@ public class CorsConfig {
      * @return
      */
     @Bean
-    public FilterRegistrationBean corsFilter() {
+    public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
@@ -31,9 +29,7 @@ public class CorsConfig {
         config.addAllowedMethod("GET");
         config.setMaxAge(3600L);
         source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new CorsFilter(source));
-        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return filterRegistrationBean;
+        return new CorsFilter(source);
     }
 
 }
