438e5c930eb5f843652fb55f50c4bb382f5a17a9
William Welling <wwelling@library.tamu.edu>
2019-04-26 10:24:42 -0500

    update manifest on specific actions, disable listener during tests

--- src/main/java/edu/tamu/iiif/service/MessageListenerService.java
+++ src/main/java/edu/tamu/iiif/service/MessageListenerService.java
@@ -10,14 +10,17 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import edu.tamu.iiif.controller.ManifestRequest;
 import edu.tamu.iiif.model.RedisManifest;
 import edu.tamu.iiif.model.repo.RedisManifestRepo;
 import edu.tamu.weaver.messaging.annotation.WeaverMessageListener;
+import edu.tamu.weaver.messaging.model.MessageAction;
 
 @Service
+@Profile("!test")
 public class MessageListenerService {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageListenerService.class);
@@ -30,7 +33,23 @@ public class MessageListenerService {
 
     @WeaverMessageListener(destination = "${iiif.messaging.channel:cap}", containerFactory = "topicContainerFactory")
     private void update(Map<String, String> message) {
-        List<RedisManifest> manifests = manifestRepo.findByPath(encode(message.get("id")));
+        MessageAction action = MessageAction.valueOf(message.get("action"));
+        switch (action) {
+        case CREATE:
+        case DELETE:
+        case REFRESH:
+        case UPDATE:
+            updateManifest(message.get("id"));
+            break;
+        case READ:
+        default:
+            break;
+        }
+
+    }
+
+    private void updateManifest(String id) {
+        List<RedisManifest> manifests = manifestRepo.findByPath(encode(id));
         manifests.stream().forEach(manifest -> {
             manifestServices.stream().filter(manifestService -> manifestService.getManifestType().equals(manifest.getType()) && manifestService.getRepository().equals(manifest.getRepository())).forEach(manifestService -> {
                 try {
