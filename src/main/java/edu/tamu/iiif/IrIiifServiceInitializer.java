12abaa82f6ebebbc9ddc76fd47f0184d444e139b
William Welling <wwelling@library.tamu.edu>
2019-05-02 11:52:52 -0500

    trying Import on initializer

--- src/main/java/edu/tamu/iiif/IrIiifServiceInitializer.java
+++ src/main/java/edu/tamu/iiif/IrIiifServiceInitializer.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+import edu.tamu.weaver.messaging.config.MessagingConfig;
 
 /**
  * Entry point to the IR IIIF service initializer.
@@ -12,7 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @author wwelling
  */
 @SpringBootApplication
-@ComponentScan(basePackages = { "edu.tamu" })
+@Import(MessagingConfig.class)
 public class IrIiifServiceInitializer extends SpringBootServletInitializer {
 
     /**
