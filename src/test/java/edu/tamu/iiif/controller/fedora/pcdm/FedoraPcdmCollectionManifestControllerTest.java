5a19b50ff158a1fb9de3fe77fdc9ea7c995d72f9
William Welling <wwelling@library.tamu.edu>
2019-05-13 16:18:17 -0500

    spring docs generated for fedora and dspace manifest generation

--- src/test/java/edu/tamu/iiif/controller/fedora/pcdm/FedoraPcdmCollectionManifestControllerTest.java
+++ src/test/java/edu/tamu/iiif/controller/fedora/pcdm/FedoraPcdmCollectionManifestControllerTest.java
@@ -5,13 +5,17 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.Resource;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 
@@ -32,8 +36,9 @@ public class FedoraPcdmCollectionManifestControllerTest extends AbstractManifest
     public void testGetManifest() throws Exception {
         String expected = readFileToString(json.getFile(), "UTF-8");
         when(fedoraPcdmCollectionManifestService.getManifest(any(ManifestRequest.class))).thenReturn(expected);
-        RequestBuilder requestBuilder = get("/fedora/collection/cars_pcdm").accept(APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        RequestBuilder requestBuilder = get("/fedora/collection/{context}", "cars_pcdm").accept(APPLICATION_JSON);
+        RestDocumentationResultHandler restDocHandler = document("fedora/collection", pathParameters(parameterWithName("context").description("The context path.")));
+        MvcResult result = mockMvc.perform(requestBuilder).andDo(restDocHandler).andReturn();
         assertEquals(expected, result.getResponse().getContentAsString());
     }
 
