664b2eb609d2294da7df01e14bf34d97c649f291
William Welling <wwelling@tamu.edu>
2023-08-04 09:50:43 -0500

    Handle REST client exception for unsupported URL (#134)
    * Handle rest client exception for unsupported URL
    
    Handles org.springframework.web.client.HttpClientErrorException: 404
    when fedora metadata for filename contain special characters.
    
    * Remove exception from error log
    
    * Use only debug to avoid double condition
    
    * Fix readme details

--- src/main/java/edu/tamu/iiif/service/AbstractManifestService.java
+++ src/main/java/edu/tamu/iiif/service/AbstractManifestService.java
@@ -164,11 +164,19 @@ public abstract class AbstractManifestService implements ManifestService {
         return createRdfModel(getRdf(url));
     }
 
-    protected String getRdf(String url) throws NotFoundException {
-        Optional<String> rdf = Optional.ofNullable(restTemplate.getForObject(url, String.class));
-        if (rdf.isPresent()) {
-            logger.debug("RDF for {}: \n{}\n", url, rdf.get());
-            return rdf.get();
+    private String getRdf(String url) throws NotFoundException {
+        try {
+            if (logger.isDebugEnabled()) {
+                logger.info("Requesting RDF for {}", url);
+            }
+            Optional<String> rdf = Optional.ofNullable(restTemplate.getForObject(url, String.class));
+            if (rdf.isPresent()) {
+                logger.debug("RDF for {}: \n{}\n", url, rdf.get());
+                return rdf.get();
+            }
+        } catch (RestClientException e) {
+            logger.error("Failed to get RDF for {}: {}", url, e.getMessage());
+            logger.debug("Error while requesting RDF for {}: {}", url, e.getMessage(), e);
         }
         throw new NotFoundException("RDF not found! " + url);
     }
